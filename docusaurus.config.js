ff2a9f21dae967becfa2c7828ef5ab39fcce38e7
Cody Hatfield <cody.hatfield@me.com>
2022-06-09 14:58:48 -0700

    Updatecopyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
       },
       footer: {
         style: "dark",
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Cody Hatfield (@codynhat)`,
       },
       prism: {
         theme: lightCodeTheme,
